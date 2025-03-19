14ae97ad4f953460a4418266db6fc93ed810d441
Ruslan Kyba <kybargr@gmail.com>
2025-03-19 22:16:16 +0200

    chore(deps): update minimal Node version to 20
    BREAKING CHANGE:  dropped support of Node version <20 as serialport version al also bumped

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores(["**/node_modules"]), {
+    extends: compat.extends("eslint:recommended", "plugin:jest/recommended", "prettier"),
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+}, {
+    files: ["tests/**/*"],
+
+    languageOptions: {
+        globals: {
+            ...globals.jest,
+        },
+    },
+}]);
